639b7c12f421b630582a67917541d0f2e199b89f
lucacaronti <carontiluca@gmail.com>
2018-12-06 16:46:19 +0100

    upgraded

--- Working Codes/New_with_library/FRONTAL/Src/main.c
+++ Working Codes/New_with_library/FRONTAL/Src/main.c
@@ -657,7 +657,7 @@ static void MX_TIM10_Init(void)
   htim10.Instance = TIM10;
   htim10.Init.Prescaler = 36;
   htim10.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim10.Init.Period = 5000;
+  htim10.Init.Period = 50000;
   htim10.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   if (HAL_TIM_Base_Init(&htim10) != HAL_OK)
   {
